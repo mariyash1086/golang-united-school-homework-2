6738433440a6dee4ae936cf69d2e1aecfa22748e
Mariyash1086 <milly10031986@gmail.com>
2022-02-20 14:35:04 +0400

    its HW2,0

--- solution.go
+++ solution.go
@@ -1,4 +1,8 @@
-package square
+package solution
+
+import (
+	"math"
+)
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
@@ -7,6 +11,21 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+type sideNameType int
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+const sideTriangle sideNameType = 3
+const sideSquare sideNameType = 4
+const sideCircle sideNameType = 0
+
+func CalcSquare(sideLen float64, sidesNum sideNameType) float64 {
+	switch sidesNum {
+	case sideTriangle:
+		return math.Pow(sideLen, 2) * math.Sqrt(3) / 4
+	case sideSquare:
+		return math.Pow(sideLen, 2)
+	case sideCircle:
+		return math.Pow(sideLen, 2) * math.Pi
+	default:
+		return 0
+	}
 }
